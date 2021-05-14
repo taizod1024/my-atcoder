beefd7b971e4f2c3c8809be9b41d06d86bdd8b3e
yamamoto.taizo <taizod1024@gmail.com>
2021-05-15 01:52:37 +0900

    定期コミット

--- src/atcoder/task/abc190_a.ts
+++ src/atcoder/task/abc190_a.ts
@@ -19,11 +19,7 @@ function main(input: string[]) {
 function entrypoint() {
     const lines: string[] = [];
     const reader = require('readline').createInterface({ input: process.stdin, output: process.stdout });
-    reader.on('line', function (line: string) {
-        lines.push(line);
-    });
-    reader.on('close', function () {
-        let input = lines; console.log(main(input));
-    });
+    reader.on('line', function (line: string) { lines.push(line); });
+    reader.on('close', function () { let input = lines; console.log(main(input)); });
 }
 entrypoint();
