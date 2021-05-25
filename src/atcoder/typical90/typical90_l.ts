8491187011fe8f0dbdbb52ec6073b2737eb1869e
yamamoto.taizo <taizod1024@gmail.com>
2021-05-26 08:21:30 +0900

    定期コミット

--- src/atcoder/typical90/typical90_l.ts
+++ src/atcoder/typical90/typical90_l.ts
@@ -2,16 +2,50 @@ export { };
 // main
 function main(input: string[]) {
     // param
-    let ans: number;
     let h: number, w: number;
     let q: number;
-    let qn: number[];
+    let qqn: number[][];
+    // init
     [h, w] = input.shift().split(" ").map(x => Number(x));
     q = Number(input.shift());
-    qn = input.map(x => Number(x));
+    qqn = input.map(x => x.split(" ").map(x => Number(x) - 1));
     // solve
+    let hw = new Array(h).fill(null).map(x => new Array(w).fill(-1));
+    for (let qx = 0; qx < q; qx++) {
+        if (qqn[qx][0] == 1) {
+            hw[qqn[qx][1]][qqn[qx][2]] = qx;
+        } else {
+            if (hw[qqn[qx][1]][qqn[qx][2]] < qx) { console.log("No"); continue; }
+            if (hw[qqn[qx][3]][qqn[qx][4]] < qx) { console.log("No"); continue; }
+            // sub task
+            let start = [qqn[qx][1], qqn[qx][2]];
+            let end = [qqn[qx][3], qqn[qx][4]];
+            let points = [start];
+            let point;
+            let next;
+            // TODO wip: テスト通らず
+            while (point = points.pop()) {
+                if (point[0] == end[0] && point[1] == end[1]) { console.log("Yes"); break; }
+                if (0 < point[0]) {
+                    next = [point[0] - 1, point[1]];
+                    if (hw[next[0]][next[1]]) points.push(next);
+                }
+                if (point[0] < h - 1) {
+                    next = [point[0] + 1, point[1]];
+                    if (hw[next[0]][next[1]]) points.push(next);
+                }
+                if (0 < point[1]) {
+                    next = [point[0], point[1] - 1];
+                    if (hw[next[0]][next[1]]) points.push(next);
+                }
+                if (point[1] < w - 1) {
+                    next = [point[0], point[1] + 1];
+                    if (hw[next[0]][next[1]]) points.push(next);
+                }
+            }
+        }
+    }
     // answer
-    console.log(ans);
     return;
 }
 // entrypoint
