3c7a60b8f7026dce717ed81d44af5823de083d6c
yamamoto.taizo <taizod1024@gmail.com>
2021-05-29 02:21:00 +0900

    定期コミット

--- src/atcoder/typical90/typical90_l.ts
+++ src/atcoder/typical90/typical90_l.ts
@@ -9,29 +9,27 @@ function main(input: string[]) {
     [h, w] = input.shift().split(" ").map(x => Number(x));
     q = Number(input.shift());
     qqn = input.map(x => x.split(" ").map(x => Number(x) - 1));
+    // WIP Int16Arrayを試す
     // solve
     let hw = new Array(h).fill(null).map(x => new Array(w).fill(-2));
     for (let qx = 0; qx < q; qx++) {
         if (qqn[qx][0] == 0) {
             hw[qqn[qx][2]][qqn[qx][1]] = -1;
         } else {
-            if (hw[qqn[qx][2]][qqn[qx][1]] == -2) {
-                console.log("No");
-                continue;
-            }
-            if (hw[qqn[qx][4]][qqn[qx][3]] == -2) {
-                console.log("No");
-                continue;
-            }
-            // sub task
             let start = [qqn[qx][1], qqn[qx][2]];
             let end = [qqn[qx][3], qqn[qx][4]];
+            if (hw[start[1]][start[0]] == -2) {
+                console.log("No");
+                continue;
+            }
+            if (hw[end[1]][end[0]] == -2) {
+                console.log("No");
+                continue;
+            }
             let points = [start];
-            let point;
-            let next;
             // // WIP ローカルテストパス、提出が通らない
             while (true) {
-                point = points.pop();
+                let point = points.pop();
                 if (!point) {
                     console.log("No");
                     break;
@@ -41,28 +39,28 @@ function main(input: string[]) {
                     break;
                 }
                 if (0 < point[0]) {
-                    next = [point[0] - 1, point[1]];
+                    let next = [point[0] - 1, point[1]];
                     if (-1 <= hw[next[1]][next[0]] && hw[next[1]][next[0]] < qx) {
                         hw[next[1]][next[0]] = qx;
                         points.push(next);
                     }
                 }
                 if (point[0] < w - 1) {
-                    next = [point[0] + 1, point[1]];
+                    let next = [point[0] + 1, point[1]];
                     if (-1 <= hw[next[1]][next[0]] && hw[next[1]][next[0]] < qx) {
                         hw[next[1]][next[0]] = qx;
                         points.push(next);
                     }
                 }
                 if (0 < point[1]) {
-                    next = [point[0], point[1] - 1];
+                    let next = [point[0], point[1] - 1];
                     if (-1 <= hw[next[1]][next[0]] && hw[next[1]][next[0]] < qx) {
                         hw[next[1]][next[0]] = qx;
                         points.push(next);
                     }
                 }
                 if (point[1] < h - 1) {
-                    next = [point[0], point[1] + 1];
+                    let next = [point[0], point[1] + 1];
                     if (-1 <= hw[next[1]][next[0]] && hw[next[1]][next[0]] < qx) {
                         hw[next[1]][next[0]] = qx;
                         points.push(next);
