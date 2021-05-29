aca69203d8870b8e0eecb43b9a9d808b11a764af
yamamoto.taizo <taizod1024@gmail.com>
2021-05-30 06:58:49 +0900

    定期コミット

--- src/atcoder/typical90/typical90_l.ts
+++ src/atcoder/typical90/typical90_l.ts
@@ -21,15 +21,15 @@ async function main() {
     let hw = new Array(h).fill(null).map(x => new Array(w).fill(-2));
     for (let qx = 0; qx < q; qx++) {
         if (qqn[qx][0] == 0) {
-            hw[qqn[qx][2]][qqn[qx][1]] = -1;
+            hw[qqn[qx][1]][qqn[qx][2]] = -1;
         } else {
             let start = [qqn[qx][1], qqn[qx][2]];
             let end = [qqn[qx][3], qqn[qx][4]];
-            if (hw[start[1]][start[0]] == -2) {
+            if (hw[start[0]][start[1]] == -2) {
                 console.log("No");
                 continue;
             }
-            if (hw[end[1]][end[0]] == -2) {
+            if (hw[end[0]][end[1]] == -2) {
                 console.log("No");
                 continue;
             }
@@ -47,29 +47,29 @@ async function main() {
                 }
                 if (0 < point[0]) {
                     let next = [point[0] - 1, point[1]];
-                    if (-1 <= hw[next[1]][next[0]] && hw[next[1]][next[0]] < qx) {
-                        hw[next[1]][next[0]] = qx;
+                    if (-1 <= hw[next[0]][next[1]] && hw[next[0]][next[1]] < qx) {
+                        hw[next[0]][next[1]] = qx;
                         points.push(next);
                     }
                 }
                 if (point[0] < w - 1) {
                     let next = [point[0] + 1, point[1]];
-                    if (-1 <= hw[next[1]][next[0]] && hw[next[1]][next[0]] < qx) {
-                        hw[next[1]][next[0]] = qx;
+                    if (-1 <= hw[next[0]][next[1]] && hw[next[0]][next[1]] < qx) {
+                        hw[next[0]][next[1]] = qx;
                         points.push(next);
                     }
                 }
                 if (0 < point[1]) {
                     let next = [point[0], point[1] - 1];
-                    if (-1 <= hw[next[1]][next[0]] && hw[next[1]][next[0]] < qx) {
-                        hw[next[1]][next[0]] = qx;
+                    if (-1 <= hw[next[0]][next[1]] && hw[next[0]][next[1]] < qx) {
+                        hw[next[0]][next[1]] = qx;
                         points.push(next);
                     }
                 }
                 if (point[1] < h - 1) {
                     let next = [point[0], point[1] + 1];
-                    if (-1 <= hw[next[1]][next[0]] && hw[next[1]][next[0]] < qx) {
-                        hw[next[1]][next[0]] = qx;
+                    if (-1 <= hw[next[0]][next[1]] && hw[next[0]][next[1]] < qx) {
+                        hw[next[0]][next[1]] = qx;
                         points.push(next);
                     }
                 }
