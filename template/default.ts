1d7d3584fff6ce885c52ec8635a2368f8d1c254d
yamamoto.taizo <taizod1024@gmail.com>
2021-06-07 14:41:47 +0900

    定期コミット

--- template/default.ts
+++ template/default.ts
@@ -2,19 +2,19 @@ export { };
 // main
 async function main() {
     // input
-    const rl = require('readline').createInterface({ input: process.stdin });
-    const readlineiter = rl[Symbol.asyncIterator]();
-    const readworditer = (async function* () { let vals = (await readlineiter.next()).value.split(" "); for (let nx = 0; nx < vals.length; nx++) yield vals[nx]; })();
-    const readline = async () => { return (await readlineiter.next()).value; };
-    const readword = async () => { return (await readworditer.next()).value; };
+    const readline = require('readline').createInterface({ input: process.stdin });
+    const readlineiter = readline[Symbol.asyncIterator]();
+    const readworditer = (async function* () { for await (const vals of readlineiter) for (const val of vals.split(" ")) yield await val; })();
+    const inputline = async () => { return (await readlineiter.next()).value; };
+    const inputword = async () => { return (await readworditer.next()).value; };
     // param
     let n: number;
     let anm: number[][];
     // init
-    n = Number((await readline()));
+    n = Number((await inputline()));
     anm = [];
     for (let nx = 0; nx < n; nx++) {
-        anm.push((await readline()).split(" ").map(x => Number(x)));
+        anm.push((await inputline()).split(" ").map(x => Number(x)));
     }
     // TODO edit the code
     // solve
