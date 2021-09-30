4a9c6b92e90f231ebd01f5e6b15e49e9ee397a1b
yamamoto.taizo <taizod1024@gmail.com>
2021-09-30 10:09:49 +0900

    定期コミット

--- src/atcoder/abc217/abc217_d.ts
+++ src/atcoder/abc217/abc217_d.ts
@@ -1,26 +1,28 @@
-import * as rl from "readline";
+// input
+import * as fs from "fs";
+const lineit = (function* () { for (const line of fs.readFileSync(process.stdin.fd, "utf8").split("\n")) yield line; })();
+const wordit = (function* () { while (true) { let line = lineit.next(); if (line.done) break; for (const word of String(line.value).split(" ")) yield word; } })();
+const charit = (function* () { while (true) { let word = wordit.next(); if (word.done) break; for (const char of String(word.value).split("")) yield char; } })();
+const readline = () => String((lineit.next()).value);
+const read = () => String((wordit.next()).value);
+const readchar = () => String((charit.next()).value);
+
+// standard template library
 import * as tstl from "tstl";
 
-// util for input
-const lineit = rl.createInterface({ input: process.stdin });
-const wordit = (async function* () { for await (const line of lineit) for (const word of line.split(" ")) yield await word; })();
-const charit = (async function* () { for await (const line of lineit) for (const word of line.split(" ")) for (const char of word.split("")) yield await char; })();
-const read = async () => String((await wordit.next()).value);
-const readchar = async () => String((await charit.next()).value);
-
 // main
-const main = async function () {
+const main = function () {
 
     // param
     let l: number, q: number;
     let cq: number[] = [], xq: number[] = [];
 
     // init
-    l = Number(await read());
-    q = Number(await read());
+    l = Number(read());
+    q = Number(read());
     for (let qx = 0; qx < q; qx++) {
-        cq.push(Number(await read()));
-        xq.push(Number(await read()));
+        cq.push(Number(read()));
+        xq.push(Number(read()));
     }
 
     // // solve
